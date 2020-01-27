3e8cc60a87a863c9d0a9bffb03e3cf1a64a6e264
VladimirMihalyuk <37588348+VladimirMihalyuk@users.noreply.github.com>
2020-01-27 22:05:17 +0300

    Added navigation with view pager and tab layout

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.weatherapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val listOfFragment = listOf(TodayFragment(), ForecastFragment() )
+    private val tabTitles = listOf("Today", "Forecast")
+    private val tabIcons = listOf(R.drawable.wb_sunny_24px, R.drawable.weather_partly_rainy)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pagerAdapter = ScreenSlidePagerAdapter(this)
+        viewPager.adapter = pagerAdapter
+
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = tabTitles[position]
+            tab.setIcon(tabIcons[position])
+            viewPager.setCurrentItem(tab.position, true)
+        }.attach()
+    }
+
+
+
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = listOfFragment.size
+
+        override fun createFragment(position: Int): Fragment = listOfFragment[position]
     }
 }
