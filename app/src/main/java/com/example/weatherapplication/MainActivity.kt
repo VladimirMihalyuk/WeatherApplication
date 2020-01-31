01cfc6d751e8a9c04515ac2bba566c142d28386e
VladimirMihalyuk <37588348+VladimirMihalyuk@users.noreply.github.com>
2020-01-31 14:59:41 +0300

    Added request of geolocation permission

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.weatherapplication
 
+import android.Manifest
+import android.content.Intent
+import android.location.Location
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -10,11 +15,19 @@ import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.weatherapplication.forecast.ForecastFragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import io.reactivex.subjects.PublishSubject
+import androidx.appcompat.app.AlertDialog
+import permissions.dispatcher.*
+import android.provider.Settings
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val listOfFragment = listOf(TodayFragment(),
         ForecastFragment()
     )
@@ -24,6 +37,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var swipeLayout:SwipeRefreshLayout
     var refreshingEvents: PublishSubject<Boolean> = PublishSubject.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -45,18 +59,76 @@ class MainActivity : AppCompatActivity() {
             refreshingEvents.onNext(true)
         }
 
-
+        getLocationWithPermissionCheck()
     }
 
     fun stopShowingRefreshing() {
         swipeLayout.isRefreshing = false
     }
 
+
+    @NeedsPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+    fun getLocation(){
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                Log.d("WTF", "$location")
+            }
+    }
+
+    @OnShowRationale(Manifest.permission.ACCESS_COARSE_LOCATION)
+    fun showRationaleForPhoneCall(request: PermissionRequest) {
+        AlertDialog.Builder(this)
+            .setTitle("Geolocation permission")
+            .setMessage("Need your location for work")
+            .setPositiveButton("Ok") { _,   _ -> request.proceed() }
+            .setNegativeButton("Cancel") { _, _ -> request.cancel() }
+            .show()
+    }
+
+    @OnPermissionDenied(Manifest.permission.ACCESS_COARSE_LOCATION)
+    fun onCameraDenied() {
+        showSnackBar()
+    }
+
+    @OnNeverAskAgain(Manifest.permission.ACCESS_COARSE_LOCATION)
+    fun onCameraNeverAskAgain() {
+        showSnackBar()
+    }
+
+    fun showSnackBar(){
+        val mySnackbar = Snackbar.make(swipeLayout, "Please open settings and turn on permission",
+            Snackbar.LENGTH_LONG)
+        mySnackbar.setAction("Open", OpenSettings())
+        mySnackbar.show()
+    }
+
+
+    private inner class OpenSettings : View.OnClickListener {
+
+        override fun onClick(v: View) {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val uri = Uri.fromParts("package", packageName, null)
+            intent.data = uri
+            startActivityForResult(intent, 0)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        getLocationWithPermissionCheck()
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        onRequestPermissionsResult(requestCode, grantResults)
+    }
+
     private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
         override fun getItemCount(): Int = listOfFragment.size
 
         override fun createFragment(position: Int): Fragment = listOfFragment[position]
-
-
     }
 }
